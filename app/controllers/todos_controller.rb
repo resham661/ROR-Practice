2b32cbb70653a08320650ce2e32c99aa97ba0f4d
resham661 <reshamgg12@gmail.com>
2023-01-30 00:37:33 +0530

    edit action, add index and links

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,6 +18,24 @@ class TodosController < ApplicationController
       @todo = Todo.find(params[:id])
     end
 
+    def edit
+      @todo = Todo.find(params[:id])
+    end
+
+    def update
+      @todo = Todo.find(params[:id])
+      if @todo.update(todo_params)
+        flash[:notice] = "Todo was successfully updated"
+        redirect_to todo_path(@todo)
+      else
+        render 'edit'
+      end
+    end
+
+    def index
+      @todos = Todo.all
+    end
+
     private
 
         def todo_params
